1ccd42cf9b10720630fc949b7598e31b0f1c9181
wasin.p <wasin.p@go-jek.com>
2020-11-27 11:18:39 +0700

    Wrap greeting into composable

--- androidApp/src/main/java/com/radityalabs/qualitymatter/androidApp/MainActivity.kt
+++ androidApp/src/main/java/com/radityalabs/qualitymatter/androidApp/MainActivity.kt
@@ -1,21 +1,29 @@
 package com.radityalabs.qualitymatter.androidApp
 
 import android.os.Bundle
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.setContent
+import com.radityalabs.qualitymatter.androidApp.ui.AppTheme
 import com.radityalabs.qualitymatter.shared.Greeting
-
-fun greet(): String {
-    return Greeting().greeting()
-}
+import androidx.compose.runtime.getValue
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AppTheme {
-                Text(greet())
+                HelloWorld()
             }
         }
     }
+}
+
+
+@Composable
+fun HelloWorld() {
+    val greeting = remember { Greeting() }
+    Text(greeting.greeting())
 }
