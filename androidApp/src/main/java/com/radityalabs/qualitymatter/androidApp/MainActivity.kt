17a45bd81f123e07904e5f5a2102b9101b615416
wasin.p <wasin.p@go-jek.com>
2020-11-27 11:28:29 +0700

    remove unused import

--- androidApp/src/main/java/com/radityalabs/qualitymatter/androidApp/MainActivity.kt
+++ androidApp/src/main/java/com/radityalabs/qualitymatter/androidApp/MainActivity.kt
@@ -3,12 +3,10 @@ package com.radityalabs.qualitymatter.androidApp
 import android.os.Bundle
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.setContent
 import com.radityalabs.qualitymatter.androidApp.ui.AppTheme
 import com.radityalabs.qualitymatter.shared.Greeting
-import androidx.compose.runtime.getValue
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
