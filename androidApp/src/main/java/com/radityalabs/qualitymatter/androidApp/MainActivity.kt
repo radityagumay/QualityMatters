d41bb962357c84b0f62b223140de41ff064da982
wasin.p <wasin.p@go-jek.com>
2020-11-27 11:08:05 +0700

    Remove unused activity

--- androidApp/src/main/java/com/radityalabs/qualitymatter/androidApp/MainActivity.kt
+++ androidApp/src/main/java/com/radityalabs/qualitymatter/androidApp/MainActivity.kt
@@ -12,9 +12,10 @@ fun greet(): String {
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        setContent {
+            AppTheme {
+                Text(greet())
+            }
+        }
     }
 }
